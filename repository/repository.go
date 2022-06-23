96b11fbce5e1d5986d8e3f74a774836540978ce6
kim -aka victor <31045060+kitorer@users.noreply.github.com>
2022-06-22 23:38:00 -0400

    CRUD

--- repository/repository.go
+++ repository/repository.go
@@ -2,4 +2,8 @@ package repository
 
 type Repository interface {
 	// TODO: create interface functions & implement them in database_repository.go
+	CreateSponsor(ctx context.Context, input *model.NewSponsor) (*model.Sponsor, error)
+	UpdateSponsor(ctx context.Context, id string, input *model.UpdatedSponsor) (*model.Sponsor, error)
+	DeleteSponsor(ctx context.Context, id string) (*model.Sponsor, error)
+	GetSponsor(ctx context.Context, id string) (*model.Sponsor, error)
 }
