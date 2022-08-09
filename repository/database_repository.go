a0863bf921915c33cfebc7c7bdd124ee1c603adb
Melanie Ehrlich <melanie6104@icloud.com>
2022-08-09 16:54:32 -0400

    Implement UpdateSponsor in schema.resolvers.go and database_repository.go

--- repository/database_repository.go
+++ repository/database_repository.go
@@ -2,8 +2,17 @@ package repository
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"github.com/KnightHacks/knighthacks_shared/database"
 	"github.com/KnightHacks/knighthacks_sponsors/graph/model"
+	"github.com/jackc/pgx/v4"
 	"github.com/jackc/pgx/v4/pgxpool"
+	"time"
+)
+
+var (
+	SponsorNotFound = errors.New("sponsor was not found")
 )
 
 //DatabaseRepository
@@ -23,9 +32,141 @@ func (r *DatabaseRepository) CreateSponsor(ctx context.Context, input *model.New
 	panic("implement me")
 }
 
+func (r *DatabaseRepository) getSponsorWithQueryable(ctx context.Context, id string, queryable database.Queryable) (*model.Sponsor, error) {
+	var event model.Sponsor
+	err := queryable.QueryRow(ctx, "SELECT id, description, name, logo, tier, website, since FROM events WHERE id = $1", id).Scan(&event.ID, &event.Description,
+		&event.Name, &event.Logo, &event.Tier, &event.Website, &event.Since)
+
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, SponsorNotFound
+		}
+		return nil, err
+	}
+
+	return &event, err
+}
+
 func (r *DatabaseRepository) UpdateSponsor(ctx context.Context, id string, input *model.UpdatedSponsor) (*model.Sponsor, error) {
-	//TODO implement me
-	panic("implement me")
+	if input.Description == nil && input.Name == nil && input.Logo == nil && input.Tier == nil && input.Website == nil && input.Since == nil {
+		return nil, fmt.Errorf("empty sponsor field")
+	}
+
+	var event *model.Sponsor
+	var err error
+	err = r.DatabasePool.BeginTxFunc(ctx, pgx.TxOptions{}, func(tx pgx.Tx) error {
+		if input.Description != nil {
+			err = r.UpdateDesc(ctx, id, *input.Description, tx)
+			if err != nil {
+				return err
+			}
+		}
+		if input.Name != nil {
+			err = r.UpdateName(ctx, id, *input.Name, tx)
+			if err != nil {
+				return err
+			}
+		}
+		if input.Logo != nil {
+			err = r.UpdateLogo(ctx, id, *input.Logo, tx)
+			if err != nil {
+				return err
+			}
+		}
+		if input.Tier != nil {
+			err := r.UpdateTier(ctx, id, *input.Tier, tx)
+			if err != nil {
+				return err
+			}
+		}
+		if input.Website != nil {
+			err := r.UpdateWebsite(ctx, id, *input.Website, tx)
+			if err != nil {
+				return err
+			}
+		}
+		if input.Since != nil {
+			err := r.UpdateSince(ctx, id, *input.Since, tx)
+			if err != nil {
+				return err
+			}
+		}
+		event, err = r.getSponsorWithQueryable(ctx, id, tx)
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return event, nil
+}
+
+func (r *DatabaseRepository) UpdateDesc(ctx context.Context, id string, SponsorDesc string, tx pgx.Tx) error {
+	commandTag, err := tx.Exec(ctx, "UPDATE events SET description = $1 WHERE id = $2", SponsorDesc, id)
+	if err != nil {
+		return err
+	}
+	if commandTag.RowsAffected() != 1 {
+		return SponsorNotFound
+	}
+	return nil
+}
+
+func (r *DatabaseRepository) UpdateName(ctx context.Context, id string, SponsorName string, tx pgx.Tx) error {
+	commandTag, err := tx.Exec(ctx, "UPDATE events SET name = $1 WHERE id = $2", SponsorName, id)
+	if err != nil {
+		return err
+	}
+	if commandTag.RowsAffected() != 1 {
+		return SponsorNotFound
+	}
+	return nil
+}
+
+func (r *DatabaseRepository) UpdateLogo(ctx context.Context, id string, SponsorLogo string, tx pgx.Tx) error {
+	commandTag, err := tx.Exec(ctx, "UPDATE events SET logo = $1 WHERE id = $2", SponsorLogo, id)
+	if err != nil {
+		return err
+	}
+	if commandTag.RowsAffected() != 1 {
+		return SponsorNotFound
+	}
+	return nil
+}
+
+func (r *DatabaseRepository) UpdateTier(ctx context.Context, id string, SponsorTier model.SubscriptionTier, tx pgx.Tx) error {
+	commandTag, err := tx.Exec(ctx, "UPDATE events SET tier = $1 WHERE id = $2", SponsorTier, id)
+	if err != nil {
+		return err
+	}
+	if commandTag.RowsAffected() != 1 {
+		return SponsorNotFound
+	}
+	return nil
+}
+
+func (r *DatabaseRepository) UpdateWebsite(ctx context.Context, id string, SponsorSite string, tx pgx.Tx) error {
+	commandTag, err := tx.Exec(ctx, "UPDATE events SET website = $1 WHERE id = $2", SponsorSite, id)
+	if err != nil {
+		return err
+	}
+	if commandTag.RowsAffected() != 1 {
+		return SponsorNotFound
+	}
+	return nil
+}
+
+func (r *DatabaseRepository) UpdateSince(ctx context.Context, id string, SponsorSince time.Time, tx pgx.Tx) error {
+	commandTag, err := tx.Exec(ctx, "UPDATE events SET since = $1 WHERE id = $2", SponsorSince, id)
+	if err != nil {
+		return err
+	}
+	if commandTag.RowsAffected() != 1 {
+		return SponsorNotFound
+	}
+	return nil
 }
 
 func (r *DatabaseRepository) DeleteSponsor(ctx context.Context, id string) (*model.Sponsor, error) {
