1971bc0714f1877b58a0288eae11368235cee6b0
Warren Snipes <contact@warrensnipes.dev>
2022-11-20 10:58:09 -0500

    fixed nil pointer dereference

--- repository/database_repository.go
+++ repository/database_repository.go
@@ -8,8 +8,8 @@ import (
 	"github.com/jackc/pgx/v4/pgxpool"
 )
 
-//DatabaseRepository
-//Implements the Repository interface's functions
+// DatabaseRepository
+// Implements the Repository interface's functions
 type DatabaseRepository struct {
 	DatabasePool *pgxpool.Pool
 }
@@ -21,30 +21,27 @@ func NewDatabaseRepository(databasePool *pgxpool.Pool) *DatabaseRepository {
 }
 
 func (r *DatabaseRepository) CreateSponsor(ctx context.Context, input *model.NewSponsor) (*model.Sponsor, error) {
-	//TODO implement me
-	var sponsorId string
 	var sponsorIdInt int
-	err := r.DatabasePool.QueryRow(ctx, "INSERT INTO sponsors (name, tier, since, description, website, logo) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id",
-		input.Name,
-		input.Tier,
-		input.Since,
-		input.Description,
-		input.Website,
-		input.Logo,
+	err := r.DatabasePool.QueryRow(ctx, "INSERT INTO sponsors (name, tier, since, description, website, logo_url) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id",
+		&input.Name,
+		&input.Tier,
+		&input.Since,
+		&input.Description,
+		&input.Website,
+		&input.Logo,
 	).Scan(&sponsorIdInt)
 	if err != nil {
 		return nil, err
 	}
-	sponsorId = strconv.Itoa(sponsorIdInt)
 
-	return &model.Sponsor {
-		ID: 			sponsorId,
-		Name: 			input.Name,
-		Tier: 			input.Tier,
-		Since: 			*input.Since,
-		Description: 	input.Description,
-		Website: 		input.Website,
-		Logo: 			input.Logo,
+	return &model.Sponsor{
+		ID:          strconv.Itoa(sponsorIdInt),
+		Name:        input.Name,
+		Tier:        input.Tier,
+		Since:       *input.Since,
+		Description: input.Description,
+		Website:     input.Website,
+		Logo:        input.Logo,
 	}, nil
 }
 
